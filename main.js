97ff791dcd3bf6cf2b132ce21792410518d5d723
ippishio <waterlava535@gmail.com>
2023-08-22 18:33:09 +0300

    changed distance

--- main.js
+++ main.js
@@ -2,16 +2,16 @@ const TIME_CHOOSE = 13
 const TIME_ACCELERATING = 25
 const TIME_READ = 15
 const RUNNING_DURATION = 21000
-const PARTICLE_COLOR = {"electron": '#0000FF', "antielectron": "#7902b5", "proton": "#FF0000"}
-const PARTICLE_NAMES = [{"name": "Бозон Хиггса", "link": "first_atom.gif", "text": ""}, {"name": "Нижний", "link": "first_atom.gif", "text": ""},
-{"name": "Верхний", "link": "first_atom.gif", "text": ""}, {"name": "Очарованный", "link": "first_atom.gif", "text": ""},
-{"name": "Истинный", "link": "first_atom.gif", "text": ""}, {"name": "Странный", "link": "first_atom.gif", "text": ""},
-{"name": "Прелестный", "link": "first_atom.gif", "text": ""}, {"name": "Глюон", "link": "first_atom.gif", "text": ""},
-{"name": "Фотон", "link": "first_atom.gif", "text": ""}, {"name": "Z Бозон", "link": "first_atom.gif", "text": ""},
-{"name": "W Бозон", "link": "first_atom.gif", "text": ""}, {"name": "Электрон", "link": "first_atom.gif", "text": ""}, 
-{"name": "Мюон", "link": "first_atom.gif", "text": ""}, {"name": "Тау", "link": "first_atom.gif", "text": ""},
-{"name": "Элейтронное Нейтрино", "link": "first_atom.gif", "text": ""}, {"name": "Мюонное Нейтрино", "link": "first_atom.gif", "text": ""},
-{"name": "Тау Нейтрино", "link": "first_atom.gif", "text": ""}
+const PARTICLE_COLOR = { "electron": '#0000FF', "antielectron": "#7902b5", "proton": "#FF0000" }
+const PARTICLE_NAMES = [{ "name": "Бозон Хиггса", "link": "first_atom.gif", "text": "" }, { "name": "Нижний", "link": "first_atom.gif", "text": "" },
+{ "name": "Верхний", "link": "first_atom.gif", "text": "" }, { "name": "Очарованный", "link": "first_atom.gif", "text": "" },
+{ "name": "Истинный", "link": "first_atom.gif", "text": "" }, { "name": "Странный", "link": "first_atom.gif", "text": "" },
+{ "name": "Прелестный", "link": "first_atom.gif", "text": "" }, { "name": "Глюон", "link": "first_atom.gif", "text": "" },
+{ "name": "Фотон", "link": "first_atom.gif", "text": "" }, { "name": "Z Бозон", "link": "first_atom.gif", "text": "" },
+{ "name": "W Бозон", "link": "first_atom.gif", "text": "" }, { "name": "Электрон", "link": "first_atom.gif", "text": "" },
+{ "name": "Мюон", "link": "first_atom.gif", "text": "" }, { "name": "Тау", "link": "first_atom.gif", "text": "" },
+{ "name": "Элейтронное Нейтрино", "link": "first_atom.gif", "text": "" }, { "name": "Мюонное Нейтрино", "link": "first_atom.gif", "text": "" },
+{ "name": "Тау Нейтрино", "link": "first_atom.gif", "text": "" }
 ]
 
 element_index = getRandomInt(17)
@@ -21,7 +21,7 @@ document.getElementsByClassName('new-particle-text')[0].innerHTML = PARTICLE_NAM
 
 var current_timer = TIME_CHOOSE
 var current_phase = 1
-const particle_id = {1: 2 }
+const particle_id = { 1: 2 }
 var particle_emoji1_color = PARTICLE_COLOR["electron"]
 var particle_emoji2_color = PARTICLE_COLOR["proton"]
 function init() {
@@ -40,7 +40,7 @@ function min(a, b) {
   return (a > b) ? b : a
 }
 
-document.getElementsByClassName("timer-header")[0].textContent = "Запуск коллайдера через: "+current_timer+"с."
+document.getElementsByClassName("timer-header")[0].textContent = "Запуск коллайдера через: " + current_timer + "с."
 document.getElementById("canvas").width = 1920;
 document.getElementById("canvas").height = 1080;
 class Particle {
@@ -51,8 +51,8 @@ class Particle {
     this.angle = angle
     this.xnext = this.x + this.speed * Math.cos(this.angle)
     this.ynext = this.y + this.speed * Math.sin(this.angle)
-    
-    
+
+
     this.angle_speed = angle_speed
     this.color = color
     this.color_change_speed = color_change_speed
@@ -64,7 +64,7 @@ class Particle {
     return (this.x >= 0 && this.x < canvas_width && this.y >= 0 && this.y < canvas_height) ? true : false
   }
   move() {
-    
+
     this.x += this.speed * Math.cos(this.angle)
     this.y += this.speed * Math.sin(this.angle)
     this.speed *= 0.99
@@ -72,9 +72,9 @@ class Particle {
     this.angle_speed *= 0.99
     this.xnext = this.x + this.speed * Math.cos(this.angle)
     this.ynext = this.y + this.speed * Math.sin(this.angle)
-    
-    
-    
+
+
+
     this.color[0] = max(0, this.color[0] - 66 * this.color_change_speed)
     this.color[1] = min(255, this.color[1] + (255 - 117) * this.color_change_speed)
     this.color[2] = max(128, this.color[2] - (245 - 128) * this.color_change_speed)
@@ -84,7 +84,7 @@ class Particle {
     ctx.lineTo(this.x, this.y)
 
     ctx.lineTo(this.xnext, this.ynext)
-    
+
   }
 }
 
@@ -95,11 +95,11 @@ var swiper = new Swiper(".swiper", {
   centeredSlides: true,
   slidesPerView: 2,
   coverflowEffect: {
-      rotate: 0,
-      stretch: 25,
-      depth: 230,
-      modifier: 1.2,
-      slideShadows: false,
+    rotate: 0,
+    stretch: 25,
+    depth: 230,
+    modifier: 1.2,
+    slideShadows: false,
   },
   navigation: {
     nextEl: ".swiper-button-next",
@@ -110,13 +110,13 @@ var swiper = new Swiper(".swiper", {
 const particles = []
 
 var new_particle_tl = anime.timeline({
-  
+
   easing: 'easeInOutExpo',
   duration: 1500,
   autoplay: false,
   begin: function () {
-      document.querySelector(".new-particle-text").style.top = "1000px"
-      document.getElementById("continue-new-particle").disabled = true
+    document.querySelector(".new-particle-text").style.top = "1000px"
+    document.getElementById("continue-new-particle").disabled = true
   }
 });
 
@@ -139,7 +139,7 @@ new_particle_tl.add({
   translateY: -1050
 })
 function new_particle_popup() {
-  
+
   new_particle_tl.play()
 }
 function new_particle_hide() {
@@ -198,7 +198,7 @@ function explosion_in_cursos_position(canvas, event) {
 
 
 
-function clear_canvas(){
+function clear_canvas() {
   ctx = document.getElementById('canvas').getContext('2d')
   ctx.clearRect(0, 0, 1920, 1080);
 }
@@ -210,7 +210,7 @@ function draw() {
     ctx.beginPath()
     color = particles[i].color
     ctx.strokeStyle = `rgba(${color[0]}, ${color[1]}, ${color[2]}, ${color[3]})`
-    
+
 
     particles[i].draw(ctx)
     ctx.stroke()
@@ -279,37 +279,37 @@ function show_collider() {
 
 
   move_collider_tl.play()
-  setTimeout(() => {  run_collider(); }, 1000);
-  
+  setTimeout(() => { run_collider(); }, 1000);
+
 }
 
-function back_points(){
-  
+function back_points() {
+
   var back_points_tl = anime.timeline({
     targets: '.selector-box',
     easing: 'easeInOutExpo',
     autoplay: false
   })
   back_points_tl.add({
-    targets: '.emoji1', 
+    targets: '.emoji1',
     opacity: [1, 0],
-    backgroundColor: particle_emoji1_color, 
+    backgroundColor: particle_emoji1_color,
     duration: 1000,
-    
+
   })
   back_points_tl.add({
-    targets: '.emoji2',  
+    targets: '.emoji2',
     opacity: [1, 0],
-    backgroundColor: particle_emoji2_color, 
+    backgroundColor: particle_emoji2_color,
     duration: 1000,
-  },'-=1000')
+  }, '-=1000')
   back_points_tl.play()
   document.getElementsByClassName("emoji1")[0].style.visibility = "visible";
-    document.getElementsByClassName("emoji2")[0].style.visibility = "visible";
+  document.getElementsByClassName("emoji2")[0].style.visibility = "visible";
 }
-  
 
-function run_collider(){
+
+function run_collider() {
   document.getElementsByClassName("emoji1")[0].style.backgroundColor = particle_emoji1_color;
   document.getElementsByClassName("emoji2")[0].style.backgroundColor = particle_emoji2_color;
   document.getElementById("EMOJI_PATH_1").setAttribute('d', "M 210 220 A 50 50 0 1 1 1120 220 A 50 50 0 1 1 210 220 ".repeat(10) + "A 50 50 0 1 1 1120 180")
@@ -323,48 +323,48 @@ function run_collider(){
     autoplay: false
   })
   run_collider_tl.add({
-    targets: '.emoji1',  
+    targets: '.emoji1',
     opacity: [0, 1],
     duration: 1000,
-    
+
   })
 
   run_collider_tl.add({
-    targets: '.emoji2',  
+    targets: '.emoji2',
     opacity: [0, 1],
     duration: 1000,
-  }) 
-  
+  })
+
   run_collider_tl.add({
     targets: '.emoji1',
     translateX: path1('x'),
     translateY: path1('y'),
-    duration: RUNNING_DURATION,   
+    duration: RUNNING_DURATION,
     backgroundColor: '#FFF',
-    easing: function(el, i, total) {
-      return function(t) {
+    easing: function (el, i, total) {
+      return function (t) {
         // return Math.pow(Math.tan(t*Math.PI/4), 3)
         return Math.pow(t, 5)
       }
     }
-      
+
   })
-  var no_delay = '-=' + RUNNING_DURATION; 
+  var no_delay = '-=' + RUNNING_DURATION;
   run_collider_tl.add({
     targets: '.emoji2',
     translateX: path2('x'),
     translateY: path2('y'),
-    duration: RUNNING_DURATION,   
+    duration: RUNNING_DURATION,
     backgroundColor: '#FFF',
-    easing: function(el, i, total) {
-      return function(t) {
+    easing: function (el, i, total) {
+      return function (t) {
         return Math.pow(t, 5)
       }
     }
-      
-  }, no_delay) 
+
+  }, no_delay)
   run_collider_tl.play()
-  run_collider_tl.finished.then(function(){
+  run_collider_tl.finished.then(function () {
     document.getElementsByClassName("emoji1")[0].style.visibility = "hidden";
     document.getElementsByClassName("emoji2")[0].style.visibility = "hidden";
     add_explosion(1400, 500, 200);
@@ -372,12 +372,12 @@ function run_collider(){
 }
 
 function phase_choose() { //phase 1
-    //hide_collider()
-    clear_canvas()
-    new_particle_hide()
+  //hide_collider()
+  clear_canvas()
+  new_particle_hide()
 }
 function phase_accelerating() { //phase 2
-    show_collider()
+  show_collider()
 }
 function phase_reading() { //phase 3
   //document.getElementById("new-particle-image").src = PARTICLE_NAMES.random()
@@ -395,20 +395,20 @@ function phase_timer_update() {
   current_timer -= 1
   switch (current_phase) {
     case 1:
-      document.getElementsByClassName("timer-header")[0].textContent = "Запуск коллайдера через: "+current_timer+"с."
-      if(!animation_started) phase_choose();
+      document.getElementsByClassName("timer-header")[0].textContent = "Запуск коллайдера через: " + current_timer + "с."
+      if (!animation_started) phase_choose();
       animation_started = true
       break;
     case 2:
-      document.getElementsByClassName("timer-header")[0].textContent = "Столкновение через: "+current_timer+"с."
-      if(!animation_started) phase_accelerating();
+      document.getElementsByClassName("timer-header")[0].textContent = "Столкновение через: " + current_timer + "с."
+      if (!animation_started) phase_accelerating();
       animation_started = true
       break;
     case 3:
-        document.getElementsByClassName("timer-header")[0].textContent = "Возврат через: "+current_timer+"с."
-        if(!animation_started) phase_reading();
+      document.getElementsByClassName("timer-header")[0].textContent = "Возврат через: " + current_timer + "с."
+      if (!animation_started) phase_reading();
       animation_started = true
-        break;
+      break;
   }
 
 
