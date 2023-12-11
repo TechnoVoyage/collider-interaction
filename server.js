a487759d9adbf1bbb5cbf99e0cd9962735cf8f22
ippishio <waterlava535@gmail.com>
2023-12-11 20:10:56 +0300

    server change

--- server.js
+++ server.js
@@ -3,8 +3,7 @@ const WebSocket = require('ws');
 const gpio = require('rpi-gpio')
 
 const wss = new WebSocket.WebSocketServer({ port: 8000 });
-const serialport = new SerialPort({ path: '/dev/ttyS0', baudRate: 100000 })
-
+const serialport = new SerialPort({ path: '/dev/ttyS0', baudRate: 100000 }) 
 gpio.setup(11, gpio.DIR_HIGH);
 
 
@@ -12,11 +11,11 @@ gpio.setup(11, gpio.DIR_HIGH);
 wss.on('connection', function connection(ws) {
     ws.on('error', console.error);
     serialport.on('data', function (data) {
-      console.log('Data:', data)
+      console.log('Data:', data.toString('HEX'))
       if(String(data) == 1) {
         gpio.write(11,1);
       }
-      ws.send(String(data))
+     ws.send(data.toString('HEX'))
     })
     ws.on('message', function message(msg) {
       if(String(msg)=="start") {
