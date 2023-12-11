1f0706bc23ae2d90fff38878d520a62057d39be9
ippishio <waterlava535@gmail.com>
2023-12-11 20:56:16 +0300

    refresh timeout fix

--- main.js
+++ main.js
@@ -514,7 +514,7 @@ uartSocket.onclose = function(e) {
     // uartSocket = new WebSocket("ws://127.0.0.1:8000")
   }, 5000);
 };
-// let refreshTimeout = setInterval(() => location.reload(),120000);
+let refreshTimeout = setInterval(() => location.reload(),120000);
 document.getElementsByTagName("body")[0].addEventListener("click", function () {
   clearInterval(refreshTimeout)
   refreshTimeout = setInterval(() => location.reload(), 120000);
